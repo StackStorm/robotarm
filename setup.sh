bcb77cbe950633485c0be0072dd06e3c45e906db
bigmstone <matthew99@gmail.com>
2017-05-04 21:49:51 -0500

    Adding requirements setup

--- setup.sh
+++ setup.sh
@@ -3,4 +3,6 @@ sudo apt-get install -y python3-pip python3-dev python-virtualenv
 virtualenv -p python3 .venv
 source ./.venv/bin/activate
 pip install -e git+https://github.com/bigmstone/robotapi.git#egg=robotapi
+pip install packaging appdirs typing
+pip install -r ./.venv/src/robotapi/requirements.txt
 deactivate
